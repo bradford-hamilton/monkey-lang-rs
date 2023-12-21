e6fcf66ca8039e52d7e6a544d1cd25fc3f8220fd
bradford lamson-scribner <brad.lamson@gmail.com>
2023-12-21 09:00:57 -0700

    feat: create Token struct and implement keywords and looking them up

--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fmt;
 
 /// Monkey's token types
@@ -103,4 +104,41 @@ impl fmt::Display for TokenType {
         };
         write!(f, "{}", printable)
     }
+}
+
+/// Token is a struct representing a Monkey token - holds a type and a literal
+#[derive(Clone)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub literal: String,
+    pub line: usize,
+}
+
+// TODO: can this be a static mapping of some sort?
+struct Keywords;
+
+impl Keywords {
+    pub fn all() -> HashMap<String, TokenType> {
+        let mut keywords = HashMap::new();
+
+        keywords.insert("func".to_owned(), TokenType::FUNCTION);
+        keywords.insert("let".to_owned(), TokenType::LET);
+        keywords.insert("const".to_owned(), TokenType::CONST);
+        keywords.insert("true".to_owned(), TokenType::TRUE);
+        keywords.insert("false".to_owned(), TokenType::FALSE);
+        keywords.insert("if".to_owned(), TokenType::IF);
+        keywords.insert("else".to_owned(), TokenType::ELSE);
+        keywords.insert("return".to_owned(), TokenType::RETURN);
+
+        keywords
+    }
+}
+
+/// look_up_identifier checks our keywords map for the scanned keyword. If it finds one, then
+/// the keyword's type is returned. If not, the user defined IDENTIFIER is returned
+pub fn look_up_identifier(identifier: &String) -> TokenType {
+    if Keywords::all().contains_key(identifier) {
+        return Keywords::all()[identifier];
+    }
+    TokenType::IDENTIFIER
 }
