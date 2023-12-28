9709dc077e10ce0387f34c10675968919c12c80c
bradford lamson-scribner <brad.lamson@gmail.com>
2023-12-28 06:35:27 -0700

    feat: finish symbol table work

--- src/symbol_table.rs
+++ src/symbol_table.rs
@@ -7,6 +7,7 @@
 use std::collections::HashMap;
 
 /// All available scopes
+#[derive(Copy, Clone)]
 pub enum SymbolScope {
     Global,
     Local,
@@ -27,6 +28,7 @@ impl SymbolScope {
     }
 }
 
+#[derive(Clone)]
 struct Symbol {
     name: String,
     scope: SymbolScope,
@@ -35,7 +37,105 @@ struct Symbol {
 
 struct SymbolTable {
     store: HashMap<String, Symbol>,
-    num_definitions: i64,
-    outer: Box<SymbolTable>,
+    num_definitions: usize,
+    outer: Option<Box<SymbolTable>>,
     free_symbols: Vec<Symbol>,
+}
+
+impl SymbolTable {
+    pub fn new() -> Self {
+        SymbolTable {
+            store: HashMap::new(),
+            num_definitions: 0,
+            outer: None,
+            free_symbols: vec![],
+        }
+    }
+
+    pub fn new_enclosed(outer: Option<Box<SymbolTable>>) -> Self {
+        SymbolTable {
+            store: HashMap::new(),
+            num_definitions: 0,
+            outer,
+            free_symbols: vec![],
+        }
+    }
+
+    /// define takes a name and creates a symbol, an index, and assigns scope. It then assigns
+    /// the symbol to the SymbolTable's store, increases num_definitions and returns the symbol.
+    fn define(&mut self, name: String) -> Symbol {
+        let scope = if self.outer.is_none() {
+            SymbolScope::Global
+        } else {
+            SymbolScope::Local
+        };
+        let sym = Symbol {
+            name: name.clone(),
+            scope,
+            index: self.num_definitions as i64,
+        };
+
+        self.store.insert(name.clone(), sym.clone());
+        self.num_definitions += 1;
+
+        sym
+    }
+
+    /// define_builtin creates and returns a symbol within builtin scope.
+    fn define_builtin(&mut self, index: i64, name: String) -> Symbol {
+        let sym = Symbol {
+            name: name.clone(),
+            index,
+            scope: SymbolScope::Builtin,
+        };
+        self.store.insert(name.clone(), sym.clone());
+
+        sym
+    }
+
+    fn define_free(&mut self, original: Symbol) -> Symbol {
+        self.free_symbols.push(original.clone());
+
+        let sym = Symbol {
+            name: original.clone().name,
+            scope: SymbolScope::Free,
+            index: (self.free_symbols.len() - 1) as i64,
+        };
+
+        self.store.insert(original.clone().name, sym.clone());
+
+        sym
+    }
+
+    /// defines a symbol with function scope.
+    fn define_function(&mut self, name: String) -> Symbol {
+        let sym = Symbol {
+            name: name.clone(),
+            scope: SymbolScope::Function,
+            index: 0,
+        };
+        self.store.insert(name, sym.clone());
+
+        sym
+    }
+
+    pub fn resolve(&mut self, name: &str) -> Option<Symbol> {
+        match self.store.get(name) {
+            Some(symbol) => Some(symbol.clone()),
+            None => match &mut self.outer {
+                Some(outer) => {
+                    let obj = outer.resolve(name);
+                    if let Some(symbol) = obj {
+                        match symbol.scope {
+                            SymbolScope::Global | SymbolScope::Builtin => Some(symbol),
+                            _ => Some(self.define_free(symbol)),
+                        }
+                    } else {
+                        None
+                    }
+                }
+                None => None,
+            },
+        }
+    }
 }
