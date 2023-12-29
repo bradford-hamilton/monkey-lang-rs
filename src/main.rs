c8d3bf30ab00992a68c95106f66c3a72986013d8
bradford lamson-scribner <brad.lamson@gmail.com>
2023-12-28 19:50:09 -0700

    feat: have main compile and print the bytecode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use ast::RootNode;
+use compiler::Compiler;
+use lexer::Lexer;
+use object::Object;
+use parser::Parser;
 use std::fs;
 
 mod ast;
@@ -17,8 +22,30 @@ fn main() {
         Ok(string) => string,
         Err(error) => panic!("error reading file {}: {}", filename, error),
     };
-    let lexer = lexer::Lexer::new(input);
-    let _parser = parser::Parser::new(lexer);
+    let lexer = Lexer::new(input);
+    let mut parser = Parser::new(lexer);
+    let program = parser.parse_program();
+
+    compile_bytecode_and_run(program);
 
     println!("heyo from the end");
+}
+
+fn compile_bytecode_and_run(root_node: RootNode) /* -> dyn Object */
+{
+    let mut compiler = Compiler::new();
+    let result = compiler.compile(&root_node);
+
+    match result {
+        Ok(_) => {
+            println!("compilation successful");
+        }
+        Err(e) => {
+            println!("compilation failed with error: {}", e);
+        }
+    }
+
+    let code = compiler.bytecode();
+
+    println!("code: {:?}", code.instructions.as_vec_u8())
 }
